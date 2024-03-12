4b94e9ec288cf4592d82003584cdf325e043079c
kim400im <min2000jae@naver.com>
2024-03-12 22:36:11 +0900

    commit all

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(   // 구글 느낌 세팅을 제공해준다.
       home: Scaffold(  // 상중하로 나눠준다, 기준점을 왼쪽 위로 한다.
         appBar: AppBar(actions: [Icon(Icons.search), Icon(Icons.content_copy_outlined), Icon(Icons.alarm)], title: Text('금호동3가'),),
-        body: SizedBox(
+        body: Row(
+          children: [
+            Expanded(child: Container(color: Colors.black,)),
+            Container(width: 100, color: Colors.blue,),
+            /*Flexible(child: Container(color: Colors.blue,), flex: 5,),
+            Flexible(child: Container(color: Colors.red,), flex: 5,),*/
+          ],
+        )
+        /*SizedBox(
           //child: TextButton(child: Text('글자'), onPressed: (){}, style: ButtonStyle(),)
               // TextButton, IconButton, ElevatedButon 등 쓰고 싶은거 쓰기
-        )
+        )*/
 
       )
     );
